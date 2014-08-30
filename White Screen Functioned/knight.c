b112eaf67ea6d3217d0b3f64a8fedb10a12fbe50
Pouya Kary <k@arendelle.org>
2014-08-30 13:43:40 +0430

    files added

---
+++ White Screen Functioned/knight.c
@@ -0,0 +1,12 @@
+
+//
+// Copyright 2014 Pouya Kary
+//
+
+#include <stdlib.h>
+
+int main () {
+
+  system ("mono '/home/kary/Dropbox/Document/Source Codes/Arendelle/Arendelle/bin/Debug/Arcade.exe' '/home/kary/Dropbox/Arendelle Glitchs/Pro Software/Kary Knight Move Optimizer/White Screen Functioned/main.arendelle'");
+
+}
